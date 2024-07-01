08bef7e6eceb73d97b109ea1441a4859c2ad690f
shrwn08 <shrwnkumar08@gmail.com>
2024-07-01 23:40:06 +0530

    completed

---
+++ spell-check/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SpellCheck from './component/SpellCheck';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheck /> 
+    </div>
+  );
+}
+
+export default App;
